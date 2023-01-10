76077f8a54888fcaa8dd991234a2157152a22913
Claus Due <claus@namelesscoder.net>
2023-01-10 15:31:14 +0100

    [TASK] Add basic metadata

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+$EM_CONF['testing'] = array (
+    'title' => 'Testing playground',
+    'description' => 'Testing playground',
+    'category' => 'misc',
+    'author' => 'FluidTYPO3 Team',
+    'author_email' => 'claus@namelesscoder.net',
+    'author_company' => '',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '1.0.0',
+    'constraints' =>
+        array (
+            'depends' =>
+                array (
+                    'php' => '7.4.0-8.2.99',
+                    'typo3' => '10.4.0-12.1.99',
+                ),
+            'conflicts' =>
+                array (
+                ),
+            'suggests' =>
+                array (
+                ),
+        ),
+    'suggests' =>
+        array (
+        ),
+    '_md5_values_when_last_written' => '',
+);
